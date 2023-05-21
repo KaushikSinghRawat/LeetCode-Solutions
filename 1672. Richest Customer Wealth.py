2305d69ad2acd26fae3b60f5aa5c72d1bc6747b4
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-05-21 20:44:48 +0530

    Create 1672. Richest Customer Wealth.py

---
+++ 1672. Richest Customer Wealth.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumWealth(self, accounts: List[List[int]]) -> int:
+        max_wealth = 0
+        #creating for loop for individually checking the sum of wealth of each account
+        #if wealth is greater than the current max_wealth, then changing value of max_wealth to be equal to the new sum
+        for i in accounts:
+            if sum(i) > max_wealth:
+                max_wealth = sum(i)
+        return max_wealth
+      
+      
+      #one line solution using list comprehensions, same logic as above
+      return max(sum(i) for i in accounts)
+      
+      #another one line solution inspired from other solutions submitted
+      return (max(map(sum, accounts))
+              
+      #
