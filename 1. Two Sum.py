37381dd5b93b96c5fc97245fac650270305ec352
KaushikSinghRawat <119755111+KaushikSinghRawat@users.noreply.github.com>
2023-05-23 12:15:13 +0530

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -14,3 +14,19 @@ class Solution:
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         return [[i, j] for i in range(len(nums)) for j in range(i + 1, len(nums)) if nums[i] + nums[j] == target][0]
+                            
+#easier to understand version of a solution by GANJINAVEEN, much faster but ok in memory efficiency:
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict={}
+        #Adding index and element of list as key and value pair to dictionary
+        #Checking whether the value(element of num) is in dictionary
+        #If not, then adding its complement to the dictionary  (Complement is any number, that if added to
+        #another number results in target number [2 is complement of 7 as adding 2 to 7 results in Target number (9)])
+        #If n is in dictionary (Is a complement of any previous iterated element),
+        #return the index number of the complement number in dictionary, and the index of the current element
+        for i,n in enumerate(nums):
+            if n not in dict:
+                dict[target-n]=i
+            else:
+                return dict[n],i
